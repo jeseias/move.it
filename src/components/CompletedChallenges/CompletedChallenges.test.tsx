354d6116e721720e2739aa4f9ce6a9e696fdd6ea
jeseias <domingosjeseias@gmail.com>
2021-05-14 11:36:04 +0100

    test: completed challenges component

--- src/components/CompletedChallenges/CompletedChallenges.test.tsx
+++ src/components/CompletedChallenges/CompletedChallenges.test.tsx
@@ -2,12 +2,22 @@ import { render, screen } from '@test-utils';
 
 import ChallengeBox from '.';
 
-const makeSut = () => render(<ChallengeBox />);
+const challengesCompleted = 5;
+
+const makeSut = () =>
+  render(<ChallengeBox />, {
+    contextStates: {
+      ChallengeState: {
+        challengesCompleted,
+      } as any,
+    } as any,
+  });
 
 describe(`${ChallengeBox.name} Component`, () => {
   it('should render as expected', () => {
     makeSut();
 
     expect(screen.getByText('Completed Challenges')).toBeInTheDocument();
+    expect(screen.getByText(challengesCompleted)).toBeInTheDocument();
   });
 });
